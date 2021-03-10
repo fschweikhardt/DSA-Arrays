1348d7454e95ef1eaa22500f9987e232ae8651e0
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-10 15:38:17 -0500

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -8,24 +8,23 @@ function main() {
     let arr = new Array();
 
     // Add an item to the array
-    arr.push(3);
-    console.log(arr);
-    arr.push(5);
-    arr.push(15);
-    arr.push(19);
-    arr.push(45);
-    arr.push(10);
-    console.log(arr);
-//Array { length: 6, _capacity: 12, ptr: 3 } the array had to resize 3x
-    arr.pop();
-    arr.pop();
-    arr.pop();
-    console.log(arr);
-//Array { length: 3, _capacity: 12, ptr: 3 } array does not resize to a smaller memory space
-
-//Print the 1st item in the array arr.
-    //arr.get(0);
-    //console.log(arr)
+        arr.push(3);
+        console.log(arr);
+        arr.push(5);
+        arr.push(15);
+        arr.push(19);
+        arr.push(45);
+        arr.push(10);
+        console.log(arr);
+    //Array { length: 6, _capacity: 12, ptr: 3 } the array had to resize 3x
+        arr.pop();
+        arr.pop();
+        arr.pop();
+        console.log(arr);
+    //Array { length: 3, _capacity: 12, ptr: 3 } array does not resize to a smaller memory space
+    //Print the 1st item in the array arr.
+        //arr.get(0);
+        //console.log(arr)
 }
 
 main();
