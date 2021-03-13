a4a2e91cc345aa2cf55abfb1df6f99fe1b7346ae
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-13 10:56:20 -0500

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -20,8 +20,29 @@ function main() {
 
     arr.push("tauhida");
 
-    console.log(arr.get(0));
+    //console.log(arr.get(0));
     
 }
 
 main();
+
+'5. URLify a string'
+
+const URLify = string => {
+    let newString = []
+    for (let i = 0; i < string.length; i++) {
+        //push each indexOf
+        //if i === '' convert to symbol
+        if (string[i] === ' ') {
+            newString[newString.length] = '%20'
+        } else {
+            newString[newString.length] = string[i]
+        }
+        //join the array to string and return
+    } 
+        return newString.join('')
+}
+
+console.log(URLify('www.thinkful.com /tauh ida parv een'))
+
+'6. Filtering an array'
